f5155291966c595521562a801854f9be76c1ebbb
Jason Zhang <xinyuanzhang29@gmail.com>
2021-10-10 11:10:33 -0400

    Poll Class

--- PollManager.java
+++ PollManager.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class PollManager {
 
   private Poll poll;
@@ -18,6 +20,36 @@ public class PollManager {
     poll.setVotes();
     if(poll.getStatus().equals(PollStatus.released)) {
       poll.setStatus(PollStatus.created);
+    }
+  }
+
+  public void ClosePoll() {
+    if(poll.getStatus().equals(PollStatus.released)) {
+      poll = null;
+    }
+  }
+
+  public void RunPoll() {
+    if(poll.getStatus().equals(PollStatus.created)) {
+      poll.setStatus(PollStatus.running);
+    }
+  }
+
+  public void ReleasePoll() {
+    if(poll.getStatus().equals(PollStatus.running)) {
+      poll.setStatus(PollStatus.released);
+    }
+  }
+
+  public void UnreleasePoll() {
+    if(poll.getStatus().equals(PollStatus.released)) {
+      poll.setStatus(PollStatus.running);
+    }
+  }
+
+  public void Vote(String participant, int choice) {
+    if(poll.getStatus().equals(PollStatus.running)) {
+      poll.setVotes(choice);
     }
   }
 }
