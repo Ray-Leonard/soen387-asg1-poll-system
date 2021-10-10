d1ab03ad3182824b901f7676b588533300b1e2f9
Jason Zhang <xinyuanzhang29@gmail.com>
2021-10-09 22:14:27 -0400

    Poll Class

--- Poll.java
+++ Poll.java
@@ -5,10 +5,57 @@ enum PollStatus {
 }
 
 public class Poll {
+
   private String name;
   private String question;
   private String[] choices;
   private int[] votes;
   private PollStatus status;
 
+  public Poll(String name, String question, String[] choices) {
+    this.name = name;
+    this.question = question;
+    this.choices = Arrays.copyOf(choices, choices.length);
+    this.votes = new int[choices.length];
+    this.status = PollStatus.created;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public String[] getChoices() {
+    String[] choices = Arrays.copyOf(this.choices, this.choices.length)
+    return choices;
+  }
+
+  public int[] getVotes() {
+    int[] votes = Arrays.copyOf(this.votes, this.votes.length);
+    return voites;
+  }
+
+  public PollStatus getStatus() {
+    return status;
+  }
+
+  public void setName(name) {
+    this.name = name;
+  }
+
+  public void setQuestion(question) {
+    this.question = question;
+  }
+
+  public void setChoices(String[] choices) {
+    this.choices = Arrays.copyOf(choices, choices.length);
+  }
+
+  public void setVotes(int idx, int num) {
+    this.votes[idx] = num;
+  }
+  
 }
