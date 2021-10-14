2edc32164ad1665fa46a738d76c6424bcf07b046
JasonZhang29 <59625134+JasonZhang29@users.noreply.github.com>
2021-10-14 17:34:54 -0400

    Poll Class

--- Poll.java
+++ Poll.java
@@ -1,9 +1,10 @@
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Poll {
+public class Poll implements Serializable{
 
   private String name;
   private String question;
