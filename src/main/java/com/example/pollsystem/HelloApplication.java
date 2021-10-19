c5cecda7940b35d97485d5013f59c7624b8124a5
Ray-Leonard <rayleonard726@gmail.com>
2021-10-18 22:36:56 -0400

    delete old files, create new repos

---
+++ src/main/java/com/example/pollsystem/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.pollsystem;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
